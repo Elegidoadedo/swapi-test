9cb0aab949d931c8d4e7330bdc787ffeee6b3e43
elegidoadedo <jmoralesmnz@gmail.com>
2018-12-18 07:09:00 +0100

    test

--- src/People/PeopleList.js
+++ src/People/PeopleList.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, {Link} from "react";
+import Swapi from '../swapi/client';
 
 export default class PeopleList extends React.Component {
+  state={
+    person: null,
+    films:null,
+  }
+  handleClick = (person) =>{
+    let id= person.films[0].split("/")
+
+    Swapi.getFilm(id[id.length-2], (response) => {
+      console.log(response)
+      this.setState({
+          films: response
+      });
+    });   
+    this.setState ({
+     person: person
+    })
+  }
+
+
   render() {
+    const{person,films} = this.state;
     if (!this.props.people || this.props.people.length <= 0) return null;
 
     const list = this.props.people.map((person, index) => (
-      <li key={index}>{person.name}</li>
+      <li key={index} onClick={() =>this.handleClick(person)}>{person.name}</li>
     ));
 
-    return <ul>{list}</ul>;
+    return (<div>
+        <ul>{list}</ul>
+        {!person || !films ? null : <div>
+          <p>Name:{person.name}</p> 
+          <p>Height:{person.height}</p>
+          <p>Films:{person.films.map(e =>{return <li>{e}</li>})}</p>
+          <p>First Film: {films.title}</p>
+        </div>}
+    </div>)
   }
 }
--- src/People/PeopleListContainer.js
+++ src/People/PeopleListContainer.js
@@ -5,7 +5,10 @@ import swapi from "../swapi/client";
 export default class PeopleListContainer extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {currentPage: 1, data: {results: []}};
+        this.state = {
+            currentPage: 1,
+            data: {results: []}
+        };
     }
 
     componentDidMount() {
@@ -20,10 +23,22 @@ export default class PeopleListContainer extends React.Component {
         });
     }
 
+    changePage(modifier) {
+        
+        this.setState({
+            currentPage: this.state.currentPage+modifier,
+        },this.loadList)     
+    }
+
     render() {
+        const {next,previous,results} = this.state.data
         return (
-            <div>
-                <PeopleList people={this.state.data.results}/>
+            <div className="people-list">
+                <PeopleList people={results}/>
+                <div className="flex">
+                    {previous ? <button onClick={()=>{this.changePage(-1)}}>Previous Page</button> : null}
+                    {next ? <button onClick={()=>{this.changePage(+1)}}>Next Page</button> : null }
+                </div>
             </div>
         );
     }
